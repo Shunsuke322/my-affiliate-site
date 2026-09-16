1da9d459b17d9920f6025c314ab946791b25d94c
Shunsuke322 <tashun0922@gmail.com>
2026-09-16 16:33:13 +0900

    Update site title and description in consts.ts

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = '楽天商品　比較ナビ';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'お買い物商品　徹底比較ナビ';
+export const SITE_DESCRIPTION = '楽天市場の人気商品を徹底比較！失敗しない選び方やおすすめランキング、お得な購入ポイントを整理して紹介する比較ナビサイトです。';
